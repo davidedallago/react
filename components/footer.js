f2054662bbf0ee02230eb0554bea72906c0dcd33
davidedallago <64363611+davidedallago@users.noreply.github.com>
2022-03-24 17:01:39 +0100

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,1 +1,23 @@
+class Footer extends React.Component {
+  render() {
+    return <div id={data.length-1} className='textcontainer contactstexcontainer fadebackground'>
+             <h1 className="containertextprop ">{this.props.title}</h1>
+             <a className="nodecoration" href={this.props.components[0].link} target="_blank">
+              <p>&#9658;<strong>{" " + this.props.components[0].subTitle}</strong> profile</p>
+             </a>
+              <a className="nodecoration" href={this.props.components[1].link} target="_blank">
+                <p>&#9658;<strong>{" " + this.props.components[1].subTitle}</strong> profile</p>
+              </a>
+              <a className="nodecoration" href={this.props.components[2].link} target="_blank">
+                <p>&#9658;<strong>{" " + this.props.components[2].subTitle}</strong> profile</p>
+              </a>
+              <p>
+                &#9658;
+                <strong>{" " + this.props.components[3].subTitle}</strong>
+                {" " + this.props.components[3].link}
+              </p>
+           </div>
+  }
+}
 
+export default class Footer;
