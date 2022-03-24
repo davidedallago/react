011bf901fb9b9f4a64182cc84fce128708420f6c
davidedallago <64363611+davidedallago@users.noreply.github.com>
2022-03-24 17:02:29 +0100

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,1 +1,33 @@
+class Navbar extends React.Component {
+  render() {
+    return <div>
+              <div className="background" value="5">
+              </div>
+              <nav>
+                <ul>
+                  <li className='selected'>HOME</li>
+                  {data.map(function(object, i){
+                  return <li>
+                          <a href={'#'+i}>{data[i].title}</a>
+                         </li>;
+                  })}
+                </ul>
+              </nav>
 
+              <div className="section">
+                <div className='homecontainer' id="navbarsec">
+                 <h1 className="hometitle">DAVIDE<br/>DAL LAGO</h1>
+                </div>
+                <div className='homecontainer' id="logocontainer">
+                  {data.map(function(object, i){
+                  return <a href={'#'+i} className="nodecoration">
+                    <h3>{data[i].title}</h3>
+                         </a>
+                  })}
+                </div>
+              </div>
+           </div>
+  }
+}
+
+export default class Navbar;
