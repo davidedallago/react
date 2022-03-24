415b18794cf69c0cae0e07959f6450b73417af4e
davidedallago <64363611+davidedallago@users.noreply.github.com>
2022-03-24 17:05:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,9 @@
 import Navbar from './components/navbar.js';
 import Section from './components/section.js';
 import Footer from './components/Footer.js';
+import {data} from './data';
 
 const projectName = 'personal-portfolio';
-
-const data = [
-  {
-    title:"PHOTO",
-    description:'Nature, portraits, fashion, shops, sports, urban or cars. Photo shoots and image manipulation using tools such as Photoshop or Lightroom.',
-    components:[{subTitle:"URBAN", link:"https://picsum.photos/id/230/300/200"},
-                {subTitle:"PORTRAIT", link:"https://picsum.photos/id/231/300/200"},
-                {subTitle:"NATURE", link:"https://picsum.photos/id/232/300/200"},
-                {subTitle:"FASHION", link:"https://picsum.photos/id/233/300/200"},
-                {subTitle:"SHOP", link:"https://picsum.photos/id/234/300/200"},
-                {subTitle:"CAR", link:"https://picsum.photos/id/235/300/200"}],
-    viewMore:"https://www.instagram.com/davide.dallago/"
-  },
-  {
-    title:"VIDEO",
-    description:"Direction and editing of corporate videos, advertising or events. In traditional landscape or portrait format to make the most of services such as Instagram Story or IGTV.",
-    components:[{subTitle:"HYPERLAPSE", link:"https://picsum.photos/id/330/300/200"},
-                {subTitle:"IG TV", link:"https://picsum.photos/id/331/300/200"},
-                {subTitle:"YOUTUBE", link:"https://picsum.photos/id/334/300/200"},
-                {subTitle:"FACEBOOK", link:"https://picsum.photos/id/335/300/200"},
-                {subTitle:"REELS", link:"https://picsum.photos/id/337/300/200"},
-                {subTitle:"TIK TOK", link:"https://picsum.photos/id/338/300/200"}],
-    viewMore:"https://www.instagram.com/davide.dallago/"
-  },
-  {
-    title:"GRAPHIC",
-    description:"Logos, textures, presentations, vector graphics and all that you need to valorize the brand and image of your company.",
-    components:[{subTitle:"LOGOS", link:"https://picsum.photos/id/430/300/200"},
-                {subTitle:"TEXTURES", link:"https://picsum.photos/id/431/300/200"},
-                {subTitle:"IMAGES", link:"https://picsum.photos/id/432/300/200"},
-                {subTitle:"VECTORIAL", link:"https://picsum.photos/id/433/300/200"},
-                {subTitle:"SHADOW", link:"https://picsum.photos/id/434/300/200"},
-                {subTitle:"PHOTOSHOP", link:"https://picsum.photos/id/435/300/200"}],
-    viewMore:"https://www.instagram.com/davide.dallago/"
-  },
-  {
-    title:"WEB DESIGN",
-    description:"Whether it's a company, a restaurant, an interior design shop or your own portfolio or blog, I have the right solution for you.",
-    components:[{subTitle:"PORTFOLIO", link:"https://picsum.photos/id/530/300/200"},
-                {subTitle:"APP", link:"https://picsum.photos/id/531/300/200"},
-                {subTitle:"BLOG", link:"https://picsum.photos/id/532/300/200"},
-                {subTitle:"E-COMMERCE", link:"https://picsum.photos/id/533/300/200"},
-                {subTitle:"MESSAGE", link:"https://picsum.photos/id/534/300/200"},
-                {subTitle:"OTHERS", link:"https://picsum.photos/id/535/300/200"}],
-    viewMore:"https://github.com/davidedallago/"
-  },
-  {
-    title:"CONTACTS",
-    description:"None",
-    components:[{subTitle:"GitHub", link:"https://github.com/davidedallago/"},
-                {subTitle:"LinkedIn", link:"https://www.linkedin.com/in/davide-dal-lago-b96873166/"},
-                {subTitle:"Instagram", link:"https://www.instagram.com/davide.dallago/"},
-                {subTitle:"E-mail", link:"d.dallago97@gmail.com"}]
-  }
-];
 
 function createApp(){
   var rows = [];
