d463f6b41cdff55b80371075578cda08e6258967
davidedallago <d.dallago97@gmail.com>
2022-03-28 17:21:13 +0200

    add

--- index.js
+++ index.js
@@ -1,59 +1,66 @@
-//import Navbar from './components/Navbar';
-//import Section from './components/Section';
-//import Footer from './components/Footer';
-
 const data = [
   {
     title:"PHOTO",
     description:'Nature, portraits, fashion, shops, sports, urban or cars. Photo shoots and image manipulation using tools such as Photoshop or Lightroom.',
-    components:[{subTitle:"URBAN", link:"https://picsum.photos/id/230/300/200"},
-                {subTitle:"PORTRAIT", link:"https://picsum.photos/id/231/300/200"},
-                {subTitle:"NATURE", link:"https://picsum.photos/id/232/300/200"},
-                {subTitle:"FASHION", link:"https://picsum.photos/id/233/300/200"},
-                {subTitle:"SHOP", link:"https://picsum.photos/id/234/300/200"},
-                {subTitle:"CAR", link:"https://picsum.photos/id/235/300/200"}],
+    components:[{subTitle:"URBAN", imgLink:"https://picsum.photos/id/230/300/200"},
+                {subTitle:"PORTRAIT", imgLink:"https://picsum.photos/id/231/300/200"},
+                {subTitle:"NATURE", imgLink:"https://picsum.photos/id/232/300/200"},
+                {subTitle:"FASHION", imgLink:"https://picsum.photos/id/233/300/200"},
+                {subTitle:"SHOP", imgLink:"https://picsum.photos/id/234/300/200"},
+                {subTitle:"CAR", imgLink:"https://picsum.photos/id/235/300/200"}],
     viewMore:"https://www.instagram.com/davide.dallago/"
   },
   {
     title:"VIDEO",
     description:"Direction and editing of corporate videos, advertising or events. In traditional landscape or portrait format to make the most of services such as Instagram Story or IGTV.",
-    components:[{subTitle:"HYPERLAPSE", link:"https://picsum.photos/id/330/300/200"},
-                {subTitle:"IG TV", link:"https://picsum.photos/id/331/300/200"},
-                {subTitle:"YOUTUBE", link:"https://picsum.photos/id/334/300/200"},
-                {subTitle:"FACEBOOK", link:"https://picsum.photos/id/335/300/200"},
-                {subTitle:"REELS", link:"https://picsum.photos/id/337/300/200"},
-                {subTitle:"TIK TOK", link:"https://picsum.photos/id/338/300/200"}],
+    components:[{subTitle:"HYPERLAPSE", imgLink:"https://picsum.photos/id/330/300/200"},
+                {subTitle:"IG TV", imgLink:"https://picsum.photos/id/331/300/200"},
+                {subTitle:"YOUTUBE", imgLink:"https://picsum.photos/id/334/300/200"},
+                {subTitle:"FACEBOOK", imgLink:"https://picsum.photos/id/335/300/200"},
+                {subTitle:"REELS", imgLink:"https://picsum.photos/id/337/300/200"},
+                {subTitle:"TIK TOK", imgLink:"https://picsum.photos/id/338/300/200"}],
     viewMore:"https://www.instagram.com/davide.dallago/"
   },
   {
     title:"GRAPHIC",
     description:"Logos, textures, presentations, vector graphics and all that you need to valorize the brand and image of your company.",
-    components:[{subTitle:"LOGOS", link:"https://picsum.photos/id/430/300/200"},
-                {subTitle:"TEXTURES", link:"https://picsum.photos/id/431/300/200"},
-                {subTitle:"IMAGES", link:"https://picsum.photos/id/432/300/200"},
-                {subTitle:"VECTORIAL", link:"https://picsum.photos/id/433/300/200"},
-                {subTitle:"SHADOW", link:"https://picsum.photos/id/434/300/200"},
-                {subTitle:"PHOTOSHOP", link:"https://picsum.photos/id/435/300/200"}],
+    components:[{subTitle:"LOGOS", imgLink:"https://picsum.photos/id/430/300/200"},
+                {subTitle:"TEXTURES", imgLink:"https://picsum.photos/id/431/300/200"},
+                {subTitle:"IMAGES", imgLink:"https://picsum.photos/id/432/300/200"},
+                {subTitle:"VECTORIAL", imgLink:"https://picsum.photos/id/433/300/200"},
+                {subTitle:"SHADOW", imgLink:"https://picsum.photos/id/434/300/200"},
+                {subTitle:"PHOTOSHOP", imgLink:"https://picsum.photos/id/435/300/200"}],
     viewMore:"https://www.instagram.com/davide.dallago/"
   },
   {
     title:"WEB DESIGN",
     description:"Whether it's a company, a restaurant, an interior design shop or your own portfolio or blog, I have the right solution for you.",
-    components:[{subTitle:"PORTFOLIO", link:"https://picsum.photos/id/530/300/200"},
-                {subTitle:"APP", link:"https://picsum.photos/id/531/300/200"},
-                {subTitle:"BLOG", link:"https://picsum.photos/id/532/300/200"},
-                {subTitle:"E-COMMERCE", link:"https://picsum.photos/id/533/300/200"},
-                {subTitle:"MESSAGE", link:"https://picsum.photos/id/534/300/200"},
-                {subTitle:"OTHERS", link:"https://picsum.photos/id/535/300/200"}],
+    components:[{subTitle:"PORTFOLIO", imgLink:"https://picsum.photos/id/530/300/200"},
+                {subTitle:"FASHION", imgLink:"https://picsum.photos/id/531/300/200"},
+                {subTitle:"BLOG", imgLink:"https://picsum.photos/id/532/300/200"},
+                {subTitle:"E-COMMERCE", imgLink:"https://picsum.photos/id/533/300/200"},
+                {subTitle:"TRIBUTE", imgLink:"https://picsum.photos/id/534/300/200"},
+                {subTitle:"LANDING", imgLink:"https://picsum.photos/id/535/300/200"}],
+    viewMore:"https://github.com/davidedallago/"
+  },
+  {
+    title:"APPLICATIONS",
+    description:"Whether it's a company, a restaurant, an interior design shop or your own portfolio or blog, I have the right solution for you.",
+    components:[{subTitle:"CALCULATOR", imgLink:"https://picsum.photos/id/530/300/200", link:"https://davidedallago.github.io/javascript-calculator/"},
+                {subTitle:"TIMER", imgLink:"https://picsum.photos/id/531/300/200", link:"https://davidedallago.github.io/timer/"},
+                {subTitle:"MARKDOWN PREVIEWER", imgLink:"https://picsum.photos/id/532/300/200", link:"https://davidedallago.github.io/markdown-previewer/"},
+                {subTitle:"QUOTE MACHINE", imgLink:"https://picsum.photos/id/533/300/200", link:"https://davidedallago.github.io/quote-machine/"},
+                {subTitle:"DRUM MACHINE", imgLink:"https://picsum.photos/id/534/300/200", link:"https://davidedallago.github.io/drum-machine/"},
+                {subTitle:"APP 6", imgLink:"https://picsum.photos/id/535/300/200"}],
     viewMore:"https://github.com/davidedallago/"
   },
   {
     title:"CONTACTS",
     description:"None",
-    components:[{subTitle:"GitHub", link:"https://github.com/davidedallago/"},
-                {subTitle:"LinkedIn", link:"https://www.linkedin.com/in/davide-dal-lago-b96873166/"},
-                {subTitle:"Instagram", link:"https://www.instagram.com/davide.dallago/"},
-                {subTitle:"E-mail", link:"d.dallago97@gmail.com"}]
+    components:[{subTitle:"GitHub", imgLink:"https://github.com/davidedallago/"},
+                {subTitle:"imgLinkedIn", imgLink:"https://www.imgLinkedin.com/in/davide-dal-lago-b96873166/"},
+                {subTitle:"Instagram", imgLink:"https://www.instagram.com/davide.dallago/"},
+                {subTitle:"E-mail", imgLink:"d.dallago97@gmail.com"}]
   }
 ];
 
@@ -101,31 +108,34 @@ class Section extends React.Component {
   render() {
     return <div className="section fadebackground" id={this.props.id}>
               <div className='elementcontainer'>
-                <div className="elementgroupprop photogroupleftdist1 photogroupbottomdist">
-                  <img className='photodim shadowprop' src={this.props.components[0].link}/>
+                <a className="elementgroupprop photogroupleftdist1 photogroupbottomdist" href={this.props.components[0].link}> 
+                  <img className='photodim shadowprop' src={this.props.components[0].imgLink}/>
                   <h3>{this.props.components[0].subTitle}</h3>
-                </div>
-                <div className="elementgroupprop photogroupleftdist2 photogroupbottomdist">
-                  <img className='photodim shadowprop' src={this.props.components[1].link}/>
+                </a>
+                <a className="elementgroupprop photogroupleftdist2 photogroupbottomdist" href={this.props.components[1].link}>
+                  <img className='photodim shadowprop' src={this.props.components[1].imgLink}/>
                   <h3>{this.props.components[1].subTitle}</h3>
-                </div>
-                <div className="elementgroupprop photogroupleftdist3 photogroupbottomdist">
-                  <img className='photodim shadowprop' src={this.props.components[2].link}/>
+                </a>
+                <a className="elementgroupprop photogroupleftdist3 photogroupbottomdist" href={this.props.components[2].link}>
+                  <img className='photodim shadowprop' src={this.props.components[2].imgLink}/>
                   <h3>{this.props.components[2].subTitle}</h3>
-                </div>
-                <div className="elementgroupprop photogroupleftdist1 photogrouptopdist">
-                  <img className='photodim shadowprop' src={this.props.components[3].link}/>
+                </a>
+                <a className="elementgroupprop photogroupleftdist1 photogrouptopdist" href={this.props.components[3].link}>
+                  <img className='photodim shadowprop' src={this.props.components[3].imgLink}/>
                   <h3>{this.props.components[3].subTitle}</h3>
-                </div>
-                <div className="elementgroupprop photogroupleftdist2 photogrouptopdist">
-                  <img className='photodim shadowprop' src={this.props.components[4].link}/>
+                </a>
+                <a className="elementgroupprop photogroupleftdist2 photogrouptopdist" href={this.props.components[4].link}>
+                  <img className='photodim shadowprop' src={this.props.components[4].imgLink}/>
                   <h3>{this.props.components[4].subTitle}</h3>
-                </div>
-                <div className="elementgroupprop photogroupleftdist3 photogrouptopdist">
-                  <img className='photodim shadowprop' src={this.props.components[5].link}/>
+                </a>
+                <a className="elementgroupprop photogroupleftdist3 photogrouptopdist" href={this.props.components[5].link}>
+                  <img className='photodim shadowprop' src={this.props.components[5].imgLink}/>
                   <h3>{this.props.components[5].subTitle}</h3>
-                </div>
+                </a>
               </div>
+              <iframe class="preview" src="http://www.youtube.com">
+                <p>Your browser does not support iframes.</p>
+              </iframe>
 
               <div className='textcontainer'>
                 <h1 className="containertextprop leftalign">{this.props.title}</h1>
@@ -140,21 +150,21 @@ class Section extends React.Component {
 
 class Footer extends React.Component {
   render() {
-    return <div id={data.length-1} className='textcontainer contactstexcontainer fadebackground'>
+    return <div id={length-1} className='textcontainer contactstexcontainer fadebackground'>
              <h1 className="containertextprop ">{this.props.title}</h1>
-             <a className="nodecoration" href={this.props.components[0].link} target="_blank">
+             <a className="nodecoration" href={this.props.components[0].imgLink} target="_blank">
               <p>&#9658;<strong>{" " + this.props.components[0].subTitle}</strong> profile</p>
              </a>
-              <a className="nodecoration" href={this.props.components[1].link} target="_blank">
+              <a className="nodecoration" href={this.props.components[1].imgLink} target="_blank">
                 <p>&#9658;<strong>{" " + this.props.components[1].subTitle}</strong> profile</p>
               </a>
-              <a className="nodecoration" href={this.props.components[2].link} target="_blank">
+              <a className="nodecoration" href={this.props.components[2].imgLink} target="_blank">
                 <p>&#9658;<strong>{" " + this.props.components[2].subTitle}</strong> profile</p>
               </a>
               <p>
                 &#9658;
                 <strong>{" " + this.props.components[3].subTitle}</strong>
-                {" " + this.props.components[3].link}
+                {" " + this.props.components[3].imgLink}
               </p>
            </div>
   }
