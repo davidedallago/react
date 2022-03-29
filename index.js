4d25812a6a542881702261ae0af41d1b7c7fd1c6
davidedallago <d.dallago97@gmail.com>
2022-03-29 16:44:05 +0200

    update

--- index.js
+++ index.js
@@ -110,16 +110,16 @@ class Footer extends React.Component {
   render() {
     return <div id={data.length-1} className='textcontainer contactstexcontainer fadebackground'>
              <h1 className="containertextprop">{this.props.title}</h1>
-             <a className="nodecoration" href={this.props.components[0].imgLink} target="_blank">
+             <a className="footerLink nodecoration" href={this.props.components[0].imgLink} target="_blank">
               <p>&#9658;<strong>{" " + this.props.components[0].subTitle}</strong> profile</p>
              </a>
-              <a className="nodecoration" href={this.props.components[1].imgLink} target="_blank">
+              <a className="footerLink nodecoration" href={this.props.components[1].imgLink} target="_blank">
                 <p>&#9658;<strong>{" " + this.props.components[1].subTitle}</strong> profile</p>
               </a>
-              <a className="nodecoration" href={this.props.components[2].imgLink} target="_blank">
+              <a className="footerLink nodecoration" href={this.props.components[2].imgLink} target="_blank">
                 <p>&#9658;<strong>{" " + this.props.components[2].subTitle}</strong> profile</p>
               </a>
-              <p>
+              <p className="footerLink">
                 &#9658;
                 <strong>{" " + this.props.components[3].subTitle}</strong>
                 {" " + this.props.components[3].imgLink}
